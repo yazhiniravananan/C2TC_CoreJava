143e545c67f5e41f674fd9111e40124c4cf83255
ADMIN <ADMIN@DESKTOP-7HIRT4P>
2023-07-28 09:40:17 +0530

    day one

--- C2TC/src/com/cg/dayone/Basics.java
+++ C2TC/src/com/cg/dayone/Basics.java
@@ -4,7 +4,15 @@ public class Basics {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int a=10;
+		int b=5;
 
+		System.out.println(a+b);//15
+		System.out.println(a-b);//5
+		System.out.println(a++);//10
+		System.out.println(a%b);//1
+		System.out.println(a/b);//2
+		System.out.println(--b);//4
 	}
 
 }
